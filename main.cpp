4bfb50d00fdebf3d53294bb5b3c3b1457fc83e6d
kinsaurralde <kinsaurralde@yahoo.com>
2019-03-22 10:18:56 -0700

    local vs aws run type

--- main.cpp
+++ main.cpp
@@ -2,18 +2,28 @@
 #include <aws/lambda-runtime/runtime.h>
 #include <iostream>
 
+#define RUN_TYPE "LOCAL"  // AWS or LOCAL
+
 using namespace aws::lambda_runtime;
 
-invocation_response my_handler(invocation_request const& request)
-{
-    std::cout << "test" << std::endl;
-    return invocation_response::success(request.payload, "application/json");
-
-    // return invocation_response::success("Hello,,,,,, World!!!!!", "application/json");
+void run_titration(std::string data) {
+    std::cout << "Data: " << data << std::endl;
 }
 
-int main()
-{
-    run_handler(my_handler);
+invocation_response my_handler(invocation_request const& request) {
+    run_titration("aws");
+    return invocation_response::success(request.payload, "application/json");
+}
+
+void local_handler() { run_titration("testing"); }
+
+int main() {
+    if (RUN_TYPE == "AWS") {
+        std::cout << "Running AWS Version" << std::endl;
+        run_handler(my_handler);
+    } else {
+        std::cout << "Running Local Testing Version" << std::endl;
+        local_handler();
+    }
     return 0;
 }
